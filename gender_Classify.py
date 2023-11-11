5f0980f0bafd0aeb9b4ebf2644b916b83d645f9e
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2023-11-11 09:07:29 -0300

    Final

--- gender_Classify.py
+++ gender_Classify.py
@@ -1,33 +1,26 @@
 
-# Pré processamento: mapenado valores numérico para as classificações
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score
 import joblib
-# Carregue seus dados em um DataFrame, por exemplo:
+
 data = pd.read_csv('./dataset/gender_classification_v7.csv')
 
-# Divida os dados em recursos (X) e rótulos (y)
 X = data[['long_hair', 'forehead_width_cm', 'forehead_height_cm', 'nose_wide', 'nose_long', 'lips_thin', 'distance_nose_to_lip_long']]
 y = data['gender']
 
-# Divida os dados em conjuntos de treinamento e teste
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Crie e treine o modelo de Regressão Logística
-model = LogisticRegression()
+model = SVC()
 model.fit(X_train, y_train)
 
-# Faça previsões no conjunto de teste
 y_pred = model.predict(X_test)
 
-# Avalie o desempenho do modelo
 accuracy = accuracy_score(y_test, y_pred)
 report = classification_report(y_test, y_pred)
 
 print("Acurácia do modelo:", accuracy)
 print("\nRelatório de classificação:\n", report)
-
 
 joblib.dump(model, 'modelo_gender_classification.pkl')
--- gender_consume.py
+++ gender_consume.py
@@ -1,52 +1,78 @@
-# from flask import Flask, request
-# import traceback
-
-# app = Flask(_name_)
-
-# @app.route("/gender", methods=['POST'])
-# def gender():
-#     book = request.jsontry
-#     try:
-
-#         return book, 200
-#     except Exception:
-#         traceback.print_exc()
-#         error = {
-#             "erro": "A classificação falhou!"
-#         }
-#         return info, 400
-
 from flask import Flask, request, jsonify
 import joblib
 
 app = Flask(__name__)
 
-# Carregue o modelo treinado
 model = joblib.load('modelo_gender_classification.pkl')
 
-@app.route('/predict', methods=['POST'])
+def realizar_previsao(data):
+    long_hair = data['long_hair']
+    forehead_width_cm = data['forehead_width_cm']
+    forehead_height_cm = data['forehead_height_cm']
+    nose_wide = data['nose_wide']
+    nose_long = data['nose_long']
+    lips_thin = data['lips_thin']
+    distance_nose_to_lip_long = data['distance_nose_to_lip_long']
+
+    prediction = model.predict([[long_hair, forehead_width_cm, forehead_height_cm, nose_wide, nose_long, lips_thin, distance_nose_to_lip_long]])[0]
+
+    gender = "Male" if prediction == 0 else "Female"
+    return gender
+
+@app.route("/", methods=['GET'])
+def main():
+    content = """
+    <h1> Modelo classificador de Gênero </h1>
+    <ul>
+        <li> 0: Male</li>
+        <li> 1: Female</li>
+    </ul>
+
+    <p> Para classificar, faça uma solicitação POST para http://127.0.0.1:5000/predict com os seguintes parâmetros:</p>
+    <ul>
+        <li> long_hair</li>
+        <li> forehead_width_cm</li>
+        <li> forehead_height_cm</li>
+        <li> nose_wide</li>
+        <li> nose_long</li>
+        <li> lips_thin</li>
+        <li> distance_nose_to_lip_long</li>
+    </ul>
+    """
+    return content
+
+@app.route("/predict", methods=['POST'])
 def predict():
     try:
         data = request.get_json()
-        long_hair = data['long_hair']
-        forehead_width_cm = data['forehead_width_cm']
-        forehead_height_cm = data['forehead_height_cm']
-        nose_wide = data['nose_wide']
-        nose_long = data['nose_long']
-        lips_thin = data['lips_thin']
-        distance_nose_to_lip_long = data['distance_nose_to_lip_long']
+        gender = realizar_previsao(data)
 
-        # Faça previsões com base nos dados de entrada
-        prediction = model.predict([[long_hair, forehead_width_cm, forehead_height_cm, nose_wide, nose_long, lips_thin, distance_nose_to_lip_long]])[0]
-
-        # Converta a previsão em texto
-        gender = "Male" if prediction == 0 else "Female"
-
-        response = {'gender': gender}
+        response = {'gender_prediction': gender}
         return jsonify(response)
 
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({'error': f'Erro na previsão: {str(e)}'})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+import requests
+import json
+
+url = "http://127.0.0.1:5000/predict"
+
+data = {
+    "long_hair": 1,
+    "forehead_width_cm": 15.0,
+    "forehead_height_cm": 10.0,
+    "nose_wide": 1,
+    "nose_long": 0,
+    "lips_thin": 1,
+    "distance_nose_to_lip_long": 5.0
+}
+
+headers = {"Content-Type": "application/json"}
+response = requests.post(url, data=json.dumps(data), headers=headers)
+
+print(response.json())
